9a241b8a07bb560e2dc394c95b99ebb7de71819e
Aaron Quasius <aquasius92@gmail.com>
2019-10-30 09:30:17 -0500

    forgot to save last time so heres my updated update. was doing some work on seeing if my recipe was holding the same values that were stored in my inventory. Had to change my parameters in my run game method, had a data type inside which i removed. Created a way for them to add their product inside of their pitcher for their recipe.

--- lemon/Recipe.cs
+++ lemon/Recipe.cs
@@ -31,6 +31,21 @@ namespace lemon
         
         public void recipeCreator(Inventory inventory)
         {
+            Console.WriteLine("How many lemons would you like to use in your pitcher?");
+            numberOfLemons = Convert.ToInt32 (Console.ReadLine());
+            Console.WriteLine(numberOfLemons + " lemons are now in your pitcher.");
+            
+            Console.WriteLine("How many sugar cubes would you like to put in your pitcher?");
+            numberOfSugarCubes = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(numberOfSugarCubes + " surgar cubes are now mixed into your pitcher!");
+
+            
+            Console.WriteLine("How many ice cubes would you like to use in your recipe?");
+            numberOfIceCubes = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(numberOfIceCubes + " ice cubes are now chilling inside your pitcher");
+
+
+
             Console.WriteLine("\n Lemons: " + inventory.lemons.Count);
             Console.WriteLine("\n Sugar: " + inventory.sugarCubes.Count);
             Console.WriteLine("\n Ice: " + inventory.iceCubes.Count);
